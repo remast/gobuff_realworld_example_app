5bb428211388ba04f36b05ab5f0bb3690979d3e4
Jan Stamer <jan.stamer@freenet.de>
2020-10-18 18:10:35 +0200

    Added model for article favorites.

---
+++ models/article_favorite.go
@@ -0,0 +1,50 @@
+package models
+
+import (
+	"encoding/json"
+
+	"github.com/gobuffalo/pop/v5"
+	"github.com/gobuffalo/validate/v3"
+	"github.com/gofrs/uuid"
+)
+
+// ArticleFavorite is used by pop to map your article_favorites database table to your go code.
+type ArticleFavorite struct {
+	User      User      `belongs_to:"user"`
+	UserID    uuid.UUID `db:"user_id"`
+	Article   Article   `belongs_to:"article"`
+	ArticleID uuid.UUID `db:"article_id"`
+}
+
+// String is not required by pop and may be deleted
+func (a ArticleFavorite) String() string {
+	ja, _ := json.Marshal(a)
+	return string(ja)
+}
+
+// ArticleFavorites is not required by pop and may be deleted
+type ArticleFavorites []ArticleFavorite
+
+// String is not required by pop and may be deleted
+func (a ArticleFavorites) String() string {
+	ja, _ := json.Marshal(a)
+	return string(ja)
+}
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
+// This method is not required and may be deleted.
+func (a *ArticleFavorite) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
+// This method is not required and may be deleted.
+func (a *ArticleFavorite) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
+// This method is not required and may be deleted.
+func (a *ArticleFavorite) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
---
+++ models/article_favorite_test.go
@@ -0,0 +1,5 @@
+package models
+
+func (ms *ModelSuite) Test_ArticleFavorite() {
+	ms.Fail("This test needs to be implemented!")
+}
