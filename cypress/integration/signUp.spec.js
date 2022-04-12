87b89349b11e80ef3d2790c780e64b67996bb1a6
Yaroslav <adolas666@gmail.com>
2022-04-12 14:29:16 +0300

    first day tests sign up

--- cypress/integration/signUp.spec.js
+++ cypress/integration/signUp.spec.js
@@ -11,7 +11,44 @@ describe('Sign up page', () => {
     cy.visit('/');
   });
 
-  it('', () => {
-    
+  it('should register a user with valid data', () => {
+    cy.intercept('POST', '/users/register').as('register');
+    cy.getByDataCy('sign-up')
+      .click();
+    cy.getByDataCy('name')
+      .type(user.username);
+    cy.getByDataCy('email')
+      .type(user.email);
+    cy.getByDataCy('password')
+      .type(user.password +'{enter}');
+
+    cy.getByDataCy('username-home')
+      .should('contain', user.username);
+    cy.getByDataCy('register-alert')
+      .should('contain', 'Welcome to RealWorld!');
+  });
+
+  it('should validate required fields', () => {
+    cy.visit('/users/register');
+    cy.getByDataCy('submit-register')
+      .click();
+    cy.contains('div', 'Name can not be blank.')
+      .should('be.visible');
+    cy.contains('div', 'Email can not be blank.')
+      .should('be.visible');
+    cy.contains('div', 'Password can not be blank.')
+      .should('be.visible');
+    cy.url()
+      .should('include', '/users/register');
+  });
+
+  it('should redirect users to sign in page by "Have an account" button', () => {
+    cy.visit('/users/register');
+    cy.getByDataCy('have-acc-btn')
+      .click();
+    cy.url()
+      .should('include', '/auth/login');
+    cy.contains('h1', 'Sign in')
+      .should('exist');
   });
 });
